2a1b1553b308c1db32c87bb7bbcd8d85c61899fb
Vuejs <vuejs@ZiMac4-4.local>
2019-04-06 13:51:17 +0200

    v-for

--- main.js
+++ main.js
@@ -1,22 +1,6 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'hola fictia vuers!',
-        pokemon: {
-                "id":25,
-                "name":"pikachu",
-                "image":"images/pokemons/pikachu.png",
-                "types":[  
-                   "electric"
-                ],
-                "abilities":[  
-                   "lightning-rod",
-                   "static"
-                ],
-                "experience":112,
-                "height":4,
-                "weight":60
-        },
         TYPE_COLOR: {
             "grass": "#78C850",
             "poison": "#A040A0",
@@ -26,6 +10,13 @@ var app = new Vue({
             "bug": "#A8B820",
             "normal": "#A8A878",
             "electric": "#F8D030"
-        }
+        },
+        pokemons: []
+    },
+    created(){
+        fetch('https://api.jsonbin.io/b/5ab37f77989617146bd6eb29')
+            .then(response => response.json())
+            .then(pokemons => this.pokemons = pokemons
+        )
     }
 });
